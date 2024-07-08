f88699839bf7d776752d098296ce107f3b10df67
Sandiroc <bsandilya1908@gmail.com>
2024-07-08 14:53:05 -0400

    correlations

--- Titanic/model.py
+++ Titanic/model.py
@@ -1,15 +1,35 @@
-import numpy
+import numpy as np
 import pandas as pd
 import sklearn as sk
+from sklearn import metrics
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def format_data():
     df = pd.read_csv("data/train.csv")
-    df = df[['Sex', 'Age', 'SibSp', 'Parch', 'Fare']]
+    df = df[['Survived', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare']]
+    df['Sex'] = df['Sex'].map({'male': 0, 'female': 1})
 
-    print(df.head())
-    
+    return df
+
+def correlations(df):
+        x = df['Sex']
+        y = df['Survived']
+        phi = metrics.matthews_corrcoef(x, y)
+        matrix = metrics.confusion_matrix(x, y)
+
+        plt.figure(figsize=(8,6))
+        sns.heatmap(matrix, annot=True, fmt='d', cmap='coolwarm', 
+                    xticklabels=['Male', 'Female'], yticklabels= ['Yes', 'No'])
+        plt.title('Sex vs Survived: Phi = ' + str("{:.2f}".format(phi)))
+        plt.xlabel('Sex')
+        plt.ylabel('Survived')
+        plt.savefig('plot/sex_vs_survived.png')
 
 
 if __name__ == "__main__":
-    format_data()
+    df = format_data()
+    print(df)
+    correlations(df['Sex'], df['Survived'])
 
